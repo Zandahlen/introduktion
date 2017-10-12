73d3764ec0f621e4ed8d5aa688c5465272a354f0
Edwin Sandahl <mango2000.edwin@gmail.com>
2017-10-12 12:18:55 +0200

    Lösningar uppgifter

--- exercises/introduction.py
+++ exercises/introduction.py
@@ -6,11 +6,19 @@ def repeat(string, num):
 
     Returnerar en ny sträng eller en tom sträng om num är negativt.
     """
-    pass
+    if num > 0:
+        return(string * num)
+    else:
+        return(string * 0)
+
 
 def bouncer(items):
     """Tar bort alla värden i items (arg 1) som evalueras till False."""
-    pass
+    result = []
+    for item in items:
+        if item:
+            result.append(item)
+    return result
 
 
 def rovarsprak(string):
@@ -19,29 +27,35 @@ def rovarsprak(string):
     * Versaler och gemener ska vara kvar enligt instoppad sträng.
     * Övriga tecken (.,*! etc.) ska vara kvar på samma position.
 
-    `Wikipedia<https://sv.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket>`_
+    `Wikipedia<https://sv.wikipedia.org/wiki/R%C3%B6varspr%C3%A5ket>`
     """
-    pass
+    import re
+    string2 = re.sub(r"([bcdfghjklmnpqrstvwxz])", r"\1o\1", string)
+    return re.sub(r"([BCDFGHJKLMNPQRSTVWXZ])", r"\1O\1", string2)
 
 
 def area(width, height):
     """Returnerar arean av en figur med bredden 'width' och höjden 'height'."""
-    pass
+    return(width * height)
 
 
 def to_seconds(time):
     """Returnerar en float `time` (timmar) till sekunder."""
-    pass
+    return(time * 60 * 60)
 
 
 def is_of_age(age):
     """Returnerar true om 'age' är större eller lika med 18, annars false."""
-    pass
+    if age >= 18:
+        return(True)
+    else:
+        return(False)
 
 
 def vowel(character):
     """Returnerar true om 'character' är en vokal, annart false."""
-    pass
+    vowels = 'auoåeiyäöAUOÅEIYÄÖ'
+    return character in vowels
 
 
 def reverse(words):
@@ -50,12 +64,12 @@ def reverse(words):
     words = "Hej på dig!" ska till exempel returnera
     strängen "!gid åp jeH".
     """
-    pass
+    return words[::-1]
 
 
 def overlapping(list1, list2):
     """Returnerar True om listorna har åtminstone ett gemensamt objekt."""
-    pass
+    return any(x in list1 for x in list2)
 
 
 def is_palindrome(words):
@@ -65,7 +79,12 @@ def is_palindrome(words):
     framlänges eller baklänges, annars false. Ett exempel på palindrom är orden
     'ni talar bra latin' som kan läsas likadant åt båda hållen.
     """
-    pass
+    words = words.lower()
+    words = words.replace(" ", "")
+    if words[::-1] == words:
+        return True
+    else:
+        return False
 
 
 def what_type(var):
@@ -82,7 +101,8 @@ def travel_price(distance, consumtion, price):
     Resan är `distance` km lång, och görs med en bil som drar `consumption`
     liter bensin per mil då bensinen kostar `price` kr per liter.
     """
-    pass
+    x = (distance * consumtion * price) / 10
+    return round(x, 2)
 
 
 def character_frequency(words):
@@ -91,4 +111,9 @@ def character_frequency(words):
     Returnerar en dictionary med varje bokstav i strängen `words` som nyckel
     till ett värde av antalet gånger bokstaven uppkommer i stängen.
     """
-    pass
+#    from string import whitespace
+#    for char in words.replace(whitespace, ''):
+    chars = {}
+    for char in words.replace(' ', ''):
+        chars[char] = chars.get(char, 0) + 1
+    return chars
--- tests/test_introduction.py
+++ tests/test_introduction.py
@@ -1,8 +1,8 @@
 import pytest
-from exercises.introduction import area, bouncer, character_frequency, is_of_age, overlapping, repeat, reverse, rovarsprak, to_seconds, travel_price, vowel
+from exercises.introduction import area, bouncer, character_frequency, is_of_age, overlapping, repeat, reverse, rovarsprak, to_seconds, travel_price, vowel, is_palindrome
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_repeat():
     assert repeat("hej", 2) == "hejhej"
     assert repeat("då", -1) == ""
@@ -10,40 +10,40 @@ def test_repeat():
     assert repeat("ab cd ef ", 2) == "ab cd ef ab cd ef "
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Not implemented yet.')
 def test_bouncer():
     assert bouncer([7, "ate", "", False, 9]) == [7, "ate", 9]
     assert bouncer(["a", "b", "c"]) == ["a", "b", "c"]
     assert bouncer([0, 0.0, 0j, {}, (), [], "hello"]) == ["hello"]
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Slow but Completed')
 def test_rovarsprak():
     assert rovarsprak("hej") == "hohejoj"
     assert rovarsprak("Hej") == "HOHejoj"
-    assert rovarsprak("TE13 är bäst.") == "TOTE13 äror bobäsostot."
+    assert rovarsprak("TE16 är bäst.") == "TOTE16 äror bobäsostot."
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_area():
     assert area(20, 20) == 400
     assert area(23.5, 24.0) == 564
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed.')
 def test_to_seconds():
     assert to_seconds(5) == 18000
     assert to_seconds(1.8) == 6480
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_is_of_age():
     assert is_of_age(12) == False
     assert is_of_age(20) == True
     assert is_of_age(17.5) == False
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_vowel():
     assert vowel('a') == True
     assert vowel('Y') == True
@@ -51,34 +51,34 @@ def test_vowel():
     assert vowel('C') == False
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_reverse():
     assert reverse('Test string') == 'gnirts tseT'
     assert reverse('Hello') == 'olleH'
     assert reverse('a') == 'a'
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_overlapping():
     assert overlapping([1,2,3], [3,4,5]) == True
     assert overlapping([1.23, 'test', 5], [1.24, 'test1', 'test']) == True
     assert overlapping(['a', 6, 'c'], ['e', 'f', 'g']) == False
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Probably Completed')
 def test_travel_price():
     assert travel_price(20, 0.6, 12) == 14.4
     assert travel_price(105.5, 0.8, 14) == 118.16
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Completed')
 def test_palindrome():
-    assert palindrome('ni talar bra latin') == True
-    assert palindrome('A car a man a maraca') == True
-    assert palindrome('hello there') == False
+    assert is_palindrome('ni talar bra latin') == True
+    assert is_palindrome('A car a man a maraca') == True
+    assert is_palindrome('hello there') == False
 
 
-@pytest.mark.skip('Not implemented yet.')
+#@pytest.mark.skip('Kinda Completed')
 def test_character_frequency():
     assert character_frequency('hej janne') == {'h' : 1, 'e' : 2, 'j' : 2, 'a' : 1, 'n' : 2}
     assert character_frequency('bananer') == {'b' : 1, 'a' : 2, 'n' : 2, 'e' : 1, 'r' : 1}
